97721e020de9fef2ab6fac566caf2088ae6548b7
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-01 19:48:52 +0700

    Menambahkan fungsi deleteNode ke dalam linked list

--- 231_singlelinkedlist/231_singlelinkedlist.cpp
+++ 231_singlelinkedlist/231_singlelinkedlist.cpp
@@ -18,10 +18,75 @@ void addNode() {
     cin >> nim;
     cout << "Masukkan Nama: ";
     cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
 
+    if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+    }
+
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
 }
 
-int main()
+bool searchNode(int nim, Node* current, Node* previous) {
+    previous = START;
+    current = START;
+    while (current != NULL && nim > current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL)
+    {
+        return false;
+    }
+    else if (current->noMhs == nim)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool deleteNode(int nim) {
+    Node* current = START;
+    Node* previous = START;
+    if (searchNode(nim, previous, current) == false)
+        return false;
+    previous->next = current->next;
+    if (current == START)
+        START = current->next;
+    return true;
+}
+
+
+    int main()
 {
     std::cout << "Hello World!\n";
 }
